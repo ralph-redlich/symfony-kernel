febb78363157d4a23dc579743f145ad1e070ad65
ralph-redlich <ralph.redlich@pantheon.io>
2025-12-26 17:01:12 +0000

    Working kernel

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+$request = Request::createFromGlobals();
+
+$routes = new RouteCollection();
+$routes->add('home', new Route('/',
+    ['_controller' => function () {
+    return new Response('Hello World!');
+}
+]));
+
+$matcher = new UrlMatcher($routes, new RequestContext());
+
+$dispatcher = new EventDispatcher();
+
+$dispatcher->addSubscriber(new RouterListener($matcher, new RequestStack()));
+
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
+$kernel = new HttpKernel($dispatcher, $controllerResolver, new RequestStack(), $argumentResolver);
+
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
+
